f618bae8f8c1626886ea4ef729cdf8c02a813464
aliyun9951438140 <huangquanjin24@gmail.com>
2025-01-07 16:35:01 +0800

    :rocket::rocket::rocket: edge_sam model autolabel

--- projects/SAM/EdgeSAM/edge_sam.py
+++ projects/SAM/EdgeSAM/edge_sam.py
@@ -2,81 +2,47 @@ import os
 import cv2
 import traceback
 import numpy as np
+import yaml
+# from PyQt5 import QtCore
+# from PyQt5.QtCore import QThread
+# from PyQt5.QtCore import QCoreApplication
 
-from PyQt5 import QtCore
-from PyQt5.QtCore import QThread
-from PyQt5.QtCore import QCoreApplication
-
-from anylabeling.app_info import __preferred_device__
-from anylabeling.utils import GenericWorker
-from anylabeling.views.labeling.shape import Shape
 from anylabeling.views.labeling.logger import logger
 from anylabeling.views.labeling.utils.opencv import qt_img_to_rgb_cv_img
 
-from .lru_cache import LRUCache
-from .model import Model
-from .types import AutoLabelingResult
-from .__base__.sam import EdgeSAMONNX
-from .__base__.clip import ChineseClipONNX
+from lru_cache import LRUCache
+from label_types import AutoLabelingResult
+from sam import EdgeSAMONNX
+# from .__base__.clip import ChineseClipONNX
 
 
-class EdgeSAM(Model):
+class EdgeSAM:
     """Segmentation model using EdgeSAM"""
+    default_output_mode = "polygon"
 
-    class Meta:
-        required_config_names = [
-            "type",
-            "name",
-            "display_name",
-            "encoder_model_path",
-            "decoder_model_path",
-        ]
-        widgets = [
-            "output_label",
-            "output_select_combobox",
-            "button_add_point",
-            "button_remove_point",
-            "button_add_rect",
-            "button_clear",
-            "button_finish_object",
-        ]
-        output_modes = {
-            "polygon": QCoreApplication.translate("Model", "Polygon"),
-            "rectangle": QCoreApplication.translate("Model", "Rectangle"),
-            "rotation": QCoreApplication.translate("Model", "Rotation"),
-        }
-        default_output_mode = "polygon"
-
-    def __init__(self, config_path, on_message) -> None:
-        # Run the parent class's init method
-        super().__init__(config_path, on_message)
-
+    def __init__(self, model_config) -> None:
+        self.set_output_mode(self.default_output_mode)
+        if isinstance(model_config, str):
+            print(model_config)
+            if not os.path.isfile(model_config):
+                raise FileNotFoundError("Model", "Config file not found: {model_config}"
+                    ).format(model_config=model_config)
+            with open(model_config, "r") as f:
+                self.config = yaml.safe_load(f)
+        elif isinstance(model_config, dict):
+            self.config = model_config
+        else:
+            raise ValueError("Unknown config type: {type}").format(type=type(model_config))
+            
         # Get encoder and decoder model paths
         encoder_model_abs_path = self.get_model_abs_path(
             self.config, "encoder_model_path"
         )
-        if not encoder_model_abs_path or not os.path.isfile(
-            encoder_model_abs_path
-        ):
-            raise FileNotFoundError(
-                QCoreApplication.translate(
-                    "Model",
-                    "Could not download or initialize encoder of EdgeSAM.",
-                )
-            )
+        
         decoder_model_abs_path = self.get_model_abs_path(
             self.config, "decoder_model_path"
         )
-        if not decoder_model_abs_path or not os.path.isfile(
-            decoder_model_abs_path
-        ):
-            raise FileNotFoundError(
-                QCoreApplication.translate(
-                    "Model",
-                    "Could not download or initialize decoder of EdgeSAM.",
-                )
-            )
-
+        
         # Load models
         self.target_length = self.config.get("target_length", 1024)
         self.model = EdgeSAMONNX(
@@ -97,28 +63,19 @@ class EdgeSAM(Model):
         self.pre_inference_worker = None
         self.stop_inference = False
 
-        # CLIP models
-        self.clip_net = None
-        clip_txt_model_path = self.config.get("txt_model_path", "")
-        clip_img_model_path = self.config.get("img_model_path", "")
-        if clip_txt_model_path and clip_img_model_path:
-            if self.config["model_type"] == "cn_clip":
-                clip_txt_model_path = self.get_model_abs_path(
-                    self.config, "txt_model_path"
-                )
-                _ = self.get_model_abs_path(self.config, "txt_extra_path")
-                clip_img_model_path = self.get_model_abs_path(
-                    self.config, "img_model_path"
-                )
-                _ = self.get_model_abs_path(self.config, "img_extra_path")
-                model_arch = self.config["model_arch"]
-                self.clip_net = ChineseClipONNX(
-                    clip_txt_model_path,
-                    clip_img_model_path,
-                    model_arch,
-                    device=__preferred_device__,
-                )
-            self.classes = self.config.get("classes", [])
+    def get_model_abs_path(self, model_config, model_path_field_name):
+        """
+        Get model absolute path from config path or download from url
+        """
+        # Try getting model path from config folder
+        model_path = model_config[model_path_field_name]
+
+        # Model path is a local path
+        model_abs_path = os.path.abspath(model_path)
+        if not os.path.exists(model_abs_path):
+            raise FileNotFoundError("Model", "Model path not found: {model_path}"
+                    ).format(model_path=model_path)
+        return model_abs_path
 
     def set_auto_labeling_marks(self, marks):
         """Set auto labeling marks"""
@@ -178,55 +135,12 @@ class EdgeSAM(Model):
                 points.append(points[0])
 
                 # Create shape
-                shape = Shape(flags={})
+                shape = []
                 for point in points:
                     point[0] = int(point[0])
                     point[1] = int(point[1])
-                    shape.add_point(QtCore.QPointF(point[0], point[1]))
-                shape.shape_type = "polygon"
-                shape.closed = True
-                shape.label = "AUTOLABEL_OBJECT"
-                shape.selected = False
+                    shape.append((point[0], point[1]))
                 shapes.append(shape)
-        elif self.output_mode in ["rectangle", "rotation"]:
-            x_min = 100000000
-            y_min = 100000000
-            x_max = 0
-            y_max = 0
-            for approx in approx_contours:
-                # Scale points
-                points = approx.reshape(-1, 2)
-                points[:, 0] = points[:, 0]
-                points[:, 1] = points[:, 1]
-                points = points.tolist()
-                if len(points) < 3:
-                    continue
-
-                # Get min/max
-                for point in points:
-                    x_min = min(x_min, point[0])
-                    y_min = min(y_min, point[1])
-                    x_max = max(x_max, point[0])
-                    y_max = max(y_max, point[1])
-
-            # Create shape
-            shape = Shape(flags={})
-            shape.add_point(QtCore.QPointF(x_min, y_min))
-            shape.add_point(QtCore.QPointF(x_max, y_min))
-            shape.add_point(QtCore.QPointF(x_max, y_max))
-            shape.add_point(QtCore.QPointF(x_min, y_max))
-            shape.shape_type = (
-                "rectangle" if self.output_mode == "rectangle" else "rotation"
-            )
-            shape.closed = True
-            if self.clip_net is not None and self.classes:
-                img = image[y_min:y_max, x_min:x_max]
-                out = self.clip_net(img, self.classes)
-                shape.cache_label = self.classes[int(np.argmax(out))]
-            shape.label = "AUTOLABEL_OBJECT"
-            shape.selected = False
-            shapes.append(shape)
-
         return shapes
 
     def predict_shapes(self, image, filename=None) -> AutoLabelingResult:
@@ -234,26 +148,23 @@ class EdgeSAM(Model):
         Predict shapes from image
         """
 
-        if image is None or not self.marks:
-            return AutoLabelingResult([], replace=False)
+        # if image is None or not self.marks:
+        #     return AutoLabelingResult([], replace=False)
 
         shapes = []
         cv_image = qt_img_to_rgb_cv_img(image, filename)
+        print(cv_image.shape)
         try:
             # Use cached image embedding if possible
             cached_data = self.image_embedding_cache.get(filename)
             if cached_data is not None:
                 image_embedding = cached_data
             else:
-                if self.stop_inference:
-                    return AutoLabelingResult([], replace=False)
                 image_embedding = self.model.encode(cv_image)
                 self.image_embedding_cache.put(
                     filename,
                     image_embedding,
                 )
-            if self.stop_inference:
-                return AutoLabelingResult([], replace=False)
             masks = self.model.predict_masks(image_embedding, self.marks)
             shapes = self.post_process(masks, cv_image)
         except Exception as e:  # noqa
@@ -264,50 +175,89 @@ class EdgeSAM(Model):
 
         result = AutoLabelingResult(shapes, replace=False)
         return result
+    
+    def set_output_mode(self, mode):
+        """
+        Set output mode
+        """
+        self.output_mode = mode
 
-    def unload(self):
-        self.stop_inference = True
-        if self.pre_inference_thread:
-            self.pre_inference_thread.quit()
+    # def unload(self):
+    #     self.stop_inference = True
+    #     if self.pre_inference_thread:
+    #         self.pre_inference_thread.quit()
 
-    def preload_worker(self, files):
-        """
-        Preload next files, run inference and cache results
-        """
-        files = files[: self.preloaded_size]
-        for filename in files:
-            if self.image_embedding_cache.find(filename):
-                continue
-            image = self.load_image_from_filename(filename)
-            if image is None:
-                continue
-            if self.stop_inference:
-                return
-            cv_image = qt_img_to_rgb_cv_img(image)
-            image_embedding = self.model.encode(cv_image)
-            self.image_embedding_cache.put(
-                filename,
-                image_embedding,
-            )
+    # def preload_worker(self, files):
+    #     """
+    #     Preload next files, run inference and cache results
+    #     """
+    #     files = files[: self.preloaded_size]
+    #     for filename in files:
+    #         if self.image_embedding_cache.find(filename):
+    #             continue
+    #         image = self.load_image_from_filename(filename)
+    #         if image is None:
+    #             continue
+    #         if self.stop_inference:
+    #             return
+    #         cv_image = qt_img_to_rgb_cv_img(image)
+    #         image_embedding = self.model.encode(cv_image)
+    #         self.image_embedding_cache.put(
+    #             filename,
+    #             image_embedding,
+    #         )
+            
+    # @staticmethod
+    # def load_image_from_filename(filename):
+    #     """Load image from labeling file and return image data and image path."""
+    #     label_file = os.path.splitext(filename)[0] + ".json"
+    #     if QFile.exists(label_file) and LabelFile.is_label_file(label_file):
+    #         try:
+    #             label_file = LabelFile(label_file)
+    #         except LabelFileError as e:
+    #             logger.error("Error reading {}: {}".format(label_file, e))
+    #             return None, None
+    #         image_data = label_file.image_data
+    #     else:
+    #         image_data = LabelFile.load_image_file(filename)
+    #     image = QImage.fromData(image_data)
+    #     if image.isNull():
+    #         logger.error("Error reading {}".format(filename))
+    #     return image
 
-    def on_next_files_changed(self, next_files):
-        """
-        Handle next files changed. This function can preload next files
-        and run inference to save time for user.
-        """
-        if (
-            self.pre_inference_thread is None
-            or not self.pre_inference_thread.isRunning()
-        ):
-            self.pre_inference_thread = QThread()
-            self.pre_inference_worker = GenericWorker(
-                self.preload_worker, next_files
-            )
-            self.pre_inference_worker.finished.connect(
-                self.pre_inference_thread.quit
-            )
-            self.pre_inference_worker.moveToThread(self.pre_inference_thread)
-            self.pre_inference_thread.started.connect(
-                self.pre_inference_worker.run
-            )
-            self.pre_inference_thread.start()
+    # def on_next_files_changed(self, next_files):
+    #     """
+    #     Handle next files changed. This function can preload next files
+    #     and run inference to save time for user.
+    #     """
+    #     if (
+    #         self.pre_inference_thread is None
+    #         or not self.pre_inference_thread.isRunning()
+    #     ):
+    #         self.pre_inference_thread = QThread()
+    #         self.pre_inference_worker = GenericWorker(
+    #             self.preload_worker, next_files
+    #         )
+    #         self.pre_inference_worker.finished.connect(
+    #             self.pre_inference_thread.quit
+    #         )
+    #         self.pre_inference_worker.moveToThread(self.pre_inference_thread)
+    #         self.pre_inference_thread.started.connect(
+    #             self.pre_inference_worker.run
+    #         )
+    #         self.pre_inference_thread.start()
+            
+if __name__ == "__main__":
+    model_config = "/Users/huangquanjin/Code/X-AnyLabeling/projects/SAM/EdgeSAM/edge_sam.yaml"
+    edge_sam = EdgeSAM(model_config)
+    edge_sam.set_output_mode("polygon")
+    # edge_sam.set_auto_labeling_marks([(100, 100), (200, 200)])
+    prompt = {'type': 'point', 'data': [168, 391], 'label': 1}
+    edge_sam.set_auto_labeling_marks([prompt])
+    result = edge_sam.predict_shapes(None, "demo.jpg")
+    print(result.shapes)
+    # 将这些polygon画到demo.jpg上
+    image = cv2.imread("demo.jpg")
+    for shape in result.shapes:
+        cv2.polylines(image, [np.array(shape)], True, (0, 0, 255), 2)
+    cv2.imwrite("demo_result.jpg", image)
